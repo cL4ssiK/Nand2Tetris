dc5e4e944d2c5829fa6ef61095d9bdf1b15063a6
Lassi Kupiainen <kupialnx@student.jyu.fi>
2025-01-19 13:31:47 +0200

    Made VMTranslator into object and able to handle folder translations.

--- Software/VMTranslator/VMTranslator.py
+++ Software/VMTranslator/VMTranslator.py
@@ -1,50 +1,100 @@
 import sys
+import os
 from Parser import Parser
 from CodeWriter import CodeWriter
 
 
+class VMTranslator:
+
+    def __init__(self, folder_name):
+        # Create object to write code to output file.
+        output_file_name = folder_name.split('.')[0] + '.asm'
+        self.cw = CodeWriter(output_file_name)
+        self.cw.config('configWriter.json')
+
+
+    def translate_file(self, file_name):
+        parser = Parser(file_name, 'config.json')
+
+        while parser.has_more_lines():
+            parser.advance()
+
+            # Remove empty lines and comments
+            if parser.current_instruction == '':
+                continue
+            
+            type = parser.command_type()
+            command = parser.arg1()
+            addr = parser.arg2()
+
+            if type == 'C_ARITHMETIC':
+                self.cw.write_arithmetic(command)
+
+            elif type == 'C_PUSH' or type == 'C_POP':
+                self.cw.write_push_pop(type, command, addr)
+
+            elif type == 'C_LABEL':
+                self.cw.write_label(command)
+
+            elif type == 'C_GOTO':
+                self.cw.write_goto(command)
+
+            elif type == 'C_IF':
+                self.cw.write_if(command)
+
+            elif type == 'C_FUNCTION':
+                self.cw.write_function(command, addr)
+
+            elif type == 'C_CALL':
+                self.cw.write_call(command, addr)
+
+            elif type == 'C_RETURN':
+                self.cw.write_return()    
+
+        parser.close()
+
+    def translate_folder(self, folder_name):
+        for filename in os.listdir(folder_name):
+            file_path = os.path.join(folder_name, filename)
+
+            # Take out folders inside folders.
+            if not os.path.isfile(file_path):
+                continue
+            # Take out every file that is not labeled .vm
+            if filename.split('.')[1] != 'vm':
+                continue
+
+            self.translate_file(file_path)
+
+
+
 #TODO: Muokkaa codewriterin aritmeettisia lauseita siten että esim lt toimii x lt y kun nyt toimii y lt x. Eli ylempänä stackissa oleva viimeisenä.
 #TODO: Make this able to handle multi file translations. 
-#TODO: Make this to ignore comments and whitespace properly.
 def main():
     
     #args = sys.argv
-    args = ['test.vm']
+    args = ['testFolderTranslation']
     if len(args) != 1:
         print("Invalid amount of arguments!")
-        return
+        sys.exit(1)
     
-    parser = Parser(args[0], 'config.json')
-    opf = args[0].split('.')[0] + '.asm'
-    cw = CodeWriter(opf)
-    cw.config('configWriter.json')
+    folder_name = args[0]
+    directory_path = os.path.join(os.getcwd(), folder_name)
 
-    while parser.has_more_lines():
-        parser.advance()
-        if parser.current_instruction == '':
-            continue
-        type = parser.command_type()
-        command = parser.arg1()
-        addr = parser.arg2()
-        if type == 'C_ARITHMETIC':
-            cw.write_arithmetic(command)
-        elif type == 'C_PUSH' or type == 'C_POP':
-            cw.write_push_pop(type, command, addr)
-        elif type == 'C_LABEL':
-            cw.write_label(command)
-        elif type == 'C_GOTO':
-            cw.write_goto(command)
-        elif type == 'C_IF':
-            cw.write_if(command)
-        elif type == 'C_FUNCTION':
-            cw.write_function(command, addr)
-        elif type == 'C_CALL':
-            cw.write_call(command, addr)
-        elif type == 'C_RETURN':
-            cw.write_return()    
+    if not (os.path.isdir(directory_path) or os.path.isfile(directory_path)):
+        print(f"Error: {directory_path} is not a valid folder.")
+        sys.exit(1)
+
+    vmt = VMTranslator(folder_name)
+
+    if os.path.isdir(directory_path):
+        vmt.translate_folder(folder_name)
+    else:
+        vmt.translate_file(folder_name)
         
-    cw.close()
-    parser.close()
+    vmt.cw.close()
+
+        
 
 if __name__ == "__main__":
     main()
